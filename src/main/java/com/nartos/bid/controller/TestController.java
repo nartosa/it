54e06ddf263bd6515a41f057ccc81adcd3eeeeae
nartos <353888506@qq.com>
2017-08-03 11:10:43 +0800

    新增bid页面跳转

--- src/main/java/com/nartos/bid/controller/TestController.java
+++ src/main/java/com/nartos/bid/controller/TestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -23,5 +24,11 @@ public class TestController
 	public List<TestVO> getAll()
 	{
 		return testService.selectAll();
+	}
+
+	@GetMapping(value = "/bidIndex")
+	public String bidIndex()
+	{
+		return "redirect:/bid";
 	}
 }
